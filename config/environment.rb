c81ce0343b25073fbde93ac305ad46601168c291
satoshi yamada <yasain0852@yahoo.co.jp>
2020-03-16 22:16:55 +0900

    test

--- config/environment.rb
+++ config/environment.rb
@@ -13,5 +13,5 @@ Rails.application.initialize!
 #   :user_name      => ENV['SENDGRID_USERNAME'],
 #   :password       => ENV['SENDGRID_PASSWORD'],
 #   :domain         => 'heroku.com',
-#   :enable_starttls_auto => true
+#   :enable_starttls_auto => truee
 # }
